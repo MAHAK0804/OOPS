9be7bc4f93d74e4352ceef789e9efc61155f640d
MAHAK <111964960+MAHAK0804@users.noreply.github.com>
2023-01-08 21:30:23 +0530

    Update and rename virtual function.cpp to virtual base class.cpp

---
+++ virtual base class.cpp
@@ -0,0 +1,76 @@
+************VIRTUAL BASE CLASS***********
+#include<iostream>
+using namespace std;
+class student
+{
+   public:
+   int m[7];
+   string n[8],s[9];
+};
+class marks:virtual public student
+{
+   int i;
+   public:
+   int getdata()
+   {
+      cout<<"enter marks"<<endl;
+      for(i=0;i<5;i++)
+      {
+         cin>>m[i];
+      }
+      
+   }
+   int display()
+   {
+      for(i=0;i<5;i++)
+      {
+      cout<<m[i]<<endl;
+      }
+   }
+};
+class subject:virtual public student
+{
+   int i;
+   public:
+   int info()
+   {
+      cout<<"enter name"<<endl;
+       for(i=0;i<5;i++)
+      {
+        cin>>n[i];
+      }
+      cout<<"enter subject"<<endl;
+      for(i=0;i<5;i++)
+      {
+        cin>>s[i];
+      }
+   }
+   int putdata()
+   {
+      for(i=0;i<5;i++)
+      {
+         cout<<n[i]<<"  "<<s[i];
+      }
+   }
+};
+class result:public marks,public subject
+{
+   int i;
+   public:
+   int printdata()
+   {
+      for(i=0;i<5;i++)
+      {
+         cout<<n[i]<<"  "<<s[i]<<"  "<<m[i]<<endl;
+      }
+   }
+};
+int main()
+{
+   result S;
+   S.getdata();
+   S.info();
+   S.putdata();
+   S.display();
+   S.printdata();
+}
