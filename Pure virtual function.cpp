93fcceb23e2559584256978cdee1df93406a5a75
MAHAK <111964960+MAHAK0804@users.noreply.github.com>
2023-01-08 21:31:06 +0530

    Create Pure virtual function.cpp

---
+++ Pure virtual function.cpp
@@ -0,0 +1,73 @@
+***********************PURE VIRTUAL FUNCTION**************************
+#include<iostream>
+using namespace std;
+class student
+{
+   public:
+   int m[7];
+   string n[8],s[9];
+};
+class marks:virtual public student
+{
+   int i;
+   public:
+   int getdata()
+   {
+      cout<<"enter marks"<<endl;
+      for(i=0;i<5;i++)
+      {
+         cin>>m[i];
+      }
+      
+   }
+  virtual int display()=0;
+   
+};
+class subject:virtual public student
+{
+   int i;
+   public:
+   int info()
+   {
+      cout<<"enter name"<<endl;
+       for(i=0;i<5;i++)
+      {
+        cin>>n[i];
+      }
+      cout<<"enter subject"<<endl;
+      for(i=0;i<5;i++)
+      {
+        cin>>s[i];
+      }
+   }
+   int putdata()
+   {
+      for(i=0;i<5;i++)
+      {
+         cout<<n[i]<<"  "<<s[i];
+      }
+   }
+};
+class result:public marks,public subject
+{
+   int i;
+   public:
+   int display()
+   {
+      for(i=0;i<5;i++)
+      {
+         cout<<n[i]<<"  "<<s[i]<<"  "<<m[i]<<endl;
+      }
+   }
+};
+int main()
+{
+   result S;
+   marks*ptr2;
+   S.getdata();
+   S.info();
+   S.putdata();
+   ptr2=&S;
+   ptr2->display();
+   
+}
